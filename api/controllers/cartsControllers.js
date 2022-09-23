64359bd65d9d8c1aa7c156b99226ac71c04bb962
J3R0N1MO <jeronimotg7@gmail.com>
2022-09-23 14:15:42 -0300

    funcionalidades de cart, terminado

--- api/controllers/cartsControllers.js
+++ api/controllers/cartsControllers.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const direcionBaseUsuarios= process.env.RUTA_DB_USER;
 const db = require('../../database/models');
-const { param } = require("../routes/cartsRoutes");
+//const { param } = require("../routes/cartsRoutes");
 
 
 function getDataU(direccion){
@@ -38,7 +38,52 @@ const cartOfId = async(req, res) => {
 };
 
 const updateCart = async(req, res) => {
-try {
+
+
+    try {
+        const userEdit = await db.User.findByPk(Number(req.params.id));
+        if(userEdit){
+            const cartOfUser = await db.Cart.destroy({where: {fk_id_user: (Number(req.params.id))}})
+
+            const previewCart = req.body;
+            const finalCart= [];
+            const noStock= [];
+            for (let i = 0; i < previewCart.length; i++) {
+                
+                let producto;
+
+                if (producto= await db.Product.findByPk(previewCart[i].fk_id_product,{raw:true})){
+                    if (producto.stock>= previewCart[i].quantity) {
+
+                    finalCart.push(previewCart[i]);
+                    
+                    }//if stock
+                    else{
+                        let fixedQuanti =  previewCart[i];
+                        fixedQuanti["quantity"]=(producto.stock);
+                        //finalCart.push(fixedQuanti);
+                        noStock.push(fixedQuanti);
+
+                    }
+            }//if existe producot
+        }//for
+        const completeCart = finalCart.concat(noStock);
+        
+
+        db.Cart.bulkCreate(completeCart);
+            
+            res.status(200).json({msg: 'Productos en Stock',
+                                    productos: finalCart,
+                                    msg2: 'Productos con stock limitado',
+                                    productos2: noStock});
+
+        }else res.status(404).json({ msg: 'No encuentra el usuario'});
+    } catch (error) {
+        res.status(500).json({Mensaje: "Server error"});
+    };
+
+/*
+    try {
     const id = req.params.id;
     const body = req.body;
 
@@ -69,19 +114,19 @@ try {
         1              5        1
         2              1        1
         3              3        2
- */
+
         carrito = cartNuevo;
         dataUsers[indiceU].cart = carrito;
         fs.writeFileSync(direcionBaseUsuarios, JSON.stringify(dataUsers));
         res.status(200).json({ CarritoNuevo: carrito});
-    }
-    else{
-        res.status(500).json({Mensaje: "Server error o id fuera de rango"});
-    };
+    
+    //else{
+      //  res.status(500).json({Mensaje: "Server error o id fuera de rango"});
+    ///};
     } catch (error) {
-        res.status(500).json("server error");
+       // res.status(500).json("server error");
     };
-}
+}*/}
 
 module.exports = {
     cartOfId,
